0003fcff6260ac7f7fa293e0a19c7ce8e2b39c46
demo <449332413@qq.com>
2017-09-23 16:19:04 +0800

    Create puputv_lightswitch.js

---
+++ puputv_lightswitch.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         puputv 开/关灯
+// @namespace    puputv
+// @version      0.1
+// @description  puputv 开/关灯
+// @author       You
+// @match        *://pupudy.com/play?*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle("#playshow_mask {display: none;z-index: 20099;position: fixed;_position: absolute;top: 0;left: 0;background: #101010;width: 100%;height: 100%;}"+
+               "#sideToolDark {display: block;z-index: 20100;position: fixed;bottom: 70px;right: 0;width: 40px;height: 40px;_position: absolute;_zoom: 1;background: #323232;color:#FFF;line-height:20px;cursor:pointer;text-align: center;}"
+               );
+    $(".sidebar").remove();
+    $(".content-wrap .content").css("margin","0px 0px");
+    $("body").append("<div id='playshow_mask'></div><div id='sideToolDark'>开/关灯</div>");
+    $(".bofangdiv").css({"position":"relative","z-index":"20100"});
+    $("#xlu").css({"position":"relative","z-index":"20100"});
+    $("#sideToolDark").click(function(){
+        $("#playshow_mask").toggle();
+    });
+    // Your code here...
+})();
